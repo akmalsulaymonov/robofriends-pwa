d37efbaec793a8d77c59eb66c2e02062fc03f61e
Akmal Sulaymonov <akmalsulaymonov@gmail.com>
2022-04-08 12:15:04 +0500

    Updates

---
+++ precache-manifest.9fab880b4b14660799abe4ba59e55105.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "813928fc7d97b628abc7",
+    "url": "/robofriends-pwa/static/css/main.6179ee0a.chunk.css"
+  },
+  {
+    "revision": "813928fc7d97b628abc7",
+    "url": "/robofriends-pwa/static/js/main.813928fc.chunk.js"
+  },
+  {
+    "revision": "8c98aab82769540efb62",
+    "url": "/robofriends-pwa/static/js/1.8c98aab8.chunk.js"
+  },
+  {
+    "revision": "89cc2dbbadea647fa1c7",
+    "url": "/robofriends-pwa/static/css/2.743a8757.chunk.css"
+  },
+  {
+    "revision": "89cc2dbbadea647fa1c7",
+    "url": "/robofriends-pwa/static/js/2.89cc2dbb.chunk.js"
+  },
+  {
+    "revision": "1113ba91467fe1d9a60b",
+    "url": "/robofriends-pwa/static/js/runtime~main.1113ba91.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-pwa/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "b60033c469ee7078e4433a7666b4e43e",
+    "url": "/robofriends-pwa/index.html"
+  }
+];
