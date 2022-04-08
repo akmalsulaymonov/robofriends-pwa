3debf1809e7615594bd2c636dfb347506d070028
Akmal Sulaymonov <akmalsulaymonov@gmail.com>
2022-04-08 12:15:29 +0500

    Optimization

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,7 +4,8 @@ const SearchBox = ({ searchfield, searchChange }) => {
   console.log('SearchBox')
   return (
     <div className='pa2'>
-      <input
+      <input 
+        aria-label='Search Robot'
         className='pa3 ba b--green bg-lightest-blue'
         type='search'
         placeholder='search robots'
